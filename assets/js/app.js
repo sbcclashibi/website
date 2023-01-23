7e531cd5efe63c3c5138ab3b3bc9fdc8529ba742
Jesse Johnson <thatguy@jessejojojohnson.com>
2023-01-23 21:54:22 +0000

    Implement document reading app
    Create test article in documents/

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,8 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js"
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js"
+import markdownIt from 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/+esm'
 
 // Set up Firebase conf
 const firebaseConfig = {
@@ -18,6 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const documentCanvas = document.getElementById("documentCanvas");
 
 const announcementsApp = createApp({
     data() {
@@ -27,16 +29,61 @@ const announcementsApp = createApp({
     },
     created() {
         getAnnouncements();
-    },
-    mounted() {
-        console.log("I've just been mounted!");
     }
 }).mount("#announcementsApp");
+
+const documentsApp = createApp({
+    data() {
+        return {
+            documentSlug: ""
+        }
+    },
+    watch: {
+        // set a watcher on documentSlug 
+        // to attempt to open a document anytime a valid slug is found
+        documentSlug(newValue, oldValue) {
+            if(newValue) { this.openDocument(); }
+        }
+    },
+    methods: {
+        openDocument() {
+            findDocumentWithSlug(this.documentSlug).then(res => {
+                const contentElement = document.getElementById("markDownContent");
+                const canvas = new bootstrap.Offcanvas('#documentCanvas');
+                if(res.status == 200) {
+                    res.text().then(md => {
+                        const m = markdownIt();
+                        contentElement.innerHTML = m.render(md);
+                    });
+                    canvas.show();
+                } else {
+                    contentElement.innerHTML = "Document Not Found :(";
+                    console.error("Could not find article with slug: " + this.documentSlug);
+                }
+            });
+        },
+        findSlug() {
+            const s = findSlugFromUrl();
+            if (s) { this.documentSlug = s; }
+        },
+        cleanUp() {
+            this.documentSlug = ""; // clear slug so we can detect new changes
+            document.getElementById("markDownContent").innerHTML = ""; // 'clear' the content of our canvas ;)
+            history.pushState("", 
+                document.title, 
+                window.location.pathname + window.location.search
+            ); // remove slug. I hate this but it works.
+        }
+    },
+    mounted() {
+        // once app is ready, attempt to find a slug so we open any referenced documents
+        this.findSlug();
+    }
+}).mount("#documentCanvas");
 
 prepareCanvas();
 
 function getAnnouncements() {
-    console.log("Getting announcements...");
     const announcementsRef = ref(database, "beta/announcements");
     onValue(announcementsRef, (snapshot) => {
         const data = snapshot.val();
@@ -48,16 +95,46 @@ function getAnnouncements() {
     });
 }
 
+function findSlugFromUrl() {
+    const urlParts = window.location.href.split("#");
+    if (urlParts.length == 2) {
+        // this means there's a hash in the url, 
+        // which means we might have a document slug
+        // we then check to see if the url hash has 'documents', the path to our documents directory
+
+        if (urlParts[1].startsWith("documents")) {
+            return urlParts[1];
+        }
+    }
+    return null;
+}
+
+function findDocumentWithSlug(slug) {
+    return fetch("/" + slug + ".md")
+        .then(response => response);
+}
+
 // Prepare Canvas for reading canvas-data attributes
 function prepareCanvas() {
-    const canvas = document.getElementById("offcanvasBottom");
-    canvas.addEventListener("show.bs.offcanvas", event => {
+    // const canvas = document.getElementById("articleCanvas");
+    // documentCanvas.addEventListener("show.bs.offcanvas", event => {
         // As an example, canvasSlug (data-canvas-slug in HTML) passes a slug to Canvas
         // We can mount a Vue app in show.bs.offcanvas to request an article from a remote source
         // and display the contents in Canvas
         //
         // Ideally, we should be able to read the page's URL, 
         // and where a valid deeplink is found, open Canvas automagically in this same way
-        console.log(event.relatedTarget.dataset.canvasSlug);
+        // console.log(event.relatedTarget.dataset.canvasSlug);
+        // UPDATE 23/01/2023: we may no longer need to pass this data attribute to documentCanvas
+    // });
+
+    documentCanvas.addEventListener("hide.bs.offcanvas", event => {
+        documentsApp.cleanUp();
+    });
+
+    window.addEventListener("hashchange", event => {
+        // We listen to changes in the URL hash, and attempt to find a document slug.
+        documentsApp.findSlug();
     });
 }
+
